9536150f22b41a2d1d2c755193718cb0540539f1
Teslimsama <107998616+Teslimsama@users.noreply.github.com>
2023-11-30 12:14:39 +0100

    done making changes to client side

---
+++ resources/views/client/shop.blade.php
@@ -0,0 +1,86 @@
+@extends('layout.master')
+@section('content')
+    <!-- brand section -->
+
+    <section class="brand_section layout_padding">
+        <div class="container">
+            <div class="heading_container">
+                <h2>
+                    Featured Brands
+                </h2>
+            </div>
+            <div class="brand_container layout_padding2">
+                <div class="box">
+                    <a href="">
+                        <div class="new">
+                            <h5>
+                                New
+                            </h5>
+                        </div>
+                        <div class="img-box">
+                            <img src="assets/images/slider-img.png" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h6 class="price">
+                                $100
+                            </h6>
+                            <h6>
+                                Chair
+                            </h6>
+                        </div>
+                    </a>
+                </div>
+                <div class="box">
+                    <a href="">
+                        <div class="img-box">
+                            <img src="assets/images/slider-img.png" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h6 class="price">
+                                $100
+                            </h6>
+                            <h6>
+                                Chair
+                            </h6>
+                        </div>
+                    </a>
+                </div>
+                <div class="box">
+                    <a href="">
+                        <div class="img-box">
+                            <img src="assets/images/slider-img.png" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h6 class="price">
+                                $100
+                            </h6>
+                            <h6>
+                                Chair
+                            </h6>
+                        </div>
+                    </a>
+                </div>
+                <div class="box">
+                    <a href="">
+                        <div class="img-box">
+                            <img src="assets/images/slider-img.png" alt="">
+                        </div>
+                        <div class="detail-box">
+                            <h6 class="price">
+                                $100
+                            </h6>
+                            <h6>
+                                Chair
+                            </h6>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <a href="" class="brand-btn">
+                See More
+            </a>
+        </div>
+    </section>
+
+    <!-- end brand section -->
+@endsection
