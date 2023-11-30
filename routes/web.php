b3ec56af857854ebfcb7d93e1a1908988b68fc9f
Abdulwahab <disu.abdulwahab@gmail.com>
2023-11-30 18:51:38 +0100

    added home controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +15,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.index');
 });
 Route::get('/about', function () {
     return view('client.about');
 });
+Route::controller(HomeController::class)->group(function () {
+    Route::get('/home', 'index')->name('home');
+    // Route::post('/home', 'storeUser')->name('home');`
+});
+// Route::controller(SalaryController::class)->group(function () {
+//     Route::get('salary/page', 'salary')->name('salary/page');
+//     Route::get('salary/add/page', 'salaryAdd')->middleware('auth')->name('salary/add/page'); // page expenses
+//     Route::post('salary/add/save', 'salarySave')->name('salary/add/save'); // save record salary
+//     Route::get('salary/edit/{id}', 'salaryEdit'); // view for edit
+//     Route::post('salary/update', 'salaryUpdate')->name('salary/update'); // update record expenses
+//     Route::put('salary/updatedata/{id}', 'update')->name('salary/update-data');
+//     Route::post('salary/delete', 'salaryDelete')->name('salary/delete'); // delete record salary
+// });
