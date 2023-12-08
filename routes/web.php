3bbc12d2041c44e040523e646008abbb6a55df89
Abdulwahab <disu.abdulwahab@gmail.com>
2023-12-08 12:42:46 +0100

    fixed the auth

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\NewPasswordController;
 use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisterController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
